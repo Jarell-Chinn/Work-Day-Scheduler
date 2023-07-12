f12d81f45f54d97164af20a74597164d445e38fd
TheLastVelo <jarellchinn44@gmail.com>
2023-07-11 17:42:12 -0700

    localstorage added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,15 @@ function displayTime() {
   var today = dayjs().format("dddd");
   currentDayEl.text(today);
 }
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+
 function dueCheck() {
   // TA suggested using a for loop using the ID's however they all have the class of time block so maybe this will be easier using the each method
   $(".time-block").each(function () {
     eventEl = $(this);
-
+    // use if statment to compare current time to the time block
+    // if the hour is less than the current time block then apply past
+    // else if the current hour is equal to the time block then apply present
+    // else the current hour is greater than time block apply future
     var eventHour = parseInt(eventEl.attr("id").split("-")[1]);
     // has to be captial H to use 24 hour time
     var currentHour = parseInt(dayjs().format("H"));
@@ -35,36 +33,29 @@ function dueCheck() {
   });
 }
 
-// $(".saveBtn").on('click', function() {
-//console.log(this);
-//figure out what hour block we are in by using $(this)
-// key names needs to match the key names for getItem localStorage
-//setItem for localStorage to save the input to the corresponding hour by using keyword 'this'
-//   })
+$(".saveBtn").on("click", function () {
+  console.log(this);
+  var hourBlock = $(this).parent().attr("id");
+  var description = $(this).siblings(".description").val();
 
-// use if statment to compare current time to the time block
-// if the hour is less than the current time block then apply future
-// else if the current hour is equal to the time block then apply present
-// else the current hour is greater than time block apply past
+  // key names needs to match the key names for getItem localStorage
+  //setItem for localStorage to save the input to the corresponding hour by using keyword 'this'
+  localStorage.setItem(hourBlock, description);
+});
 
-// remember have remove class as well as add
-
-// getItem for local storage to pull previously stored content
-// separate keys in local storage for each hour
-// create for loop for hour 9-17 to check local storage
-// check localstorage withing the loop
-// if there is storage display the (value)
-//    item in the text area
-// use id selector for the text area
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
+function loadActivites() {
+  $(".time-block").each(function () {
+    var hourBlock = $(this).attr("id");
+    var description = localStorage.getItem(hourBlock);
+    if (description) {
+      $(this).find(".description").val(description);
+    }
+  });
+}
 
 // create var for dayjs to get day and time
 // $("currentDay").text(dayjsvar)
 displayTime();
 setInterval(displayTime, 1000);
 setInterval(dueCheck, 1000);
+loadActivites();
